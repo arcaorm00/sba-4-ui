e59548331ac5c0a03123a660575912ae310d0b65
salt <arcaorm00@gmail.com>
2020-10-13 11:32:03 +0900

    11:31

---
+++ src/pages/Main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { About, Contact, Events, Home, Products, Error } from '../components'
+// {}: ../components에서 일부만 가져온다. (메모리 점유율을 줄이기 위함)
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+
+const Main = () => <>
+    <BrowserRouter>
+        <div className="main">
+            <switch>
+                <Route exact path="/" component={Home}/> 
+                <Route path="/about" component={About}/>
+                <Redirect from={"/history"} to={"/about/history"}/>
+                <Redirect from={"/service"} to={"/about/service"}/>
+                <Redirect from={"/location"} to={"/about/location"}/>
+                <Route path="/contact" component={Contact}/>
+                <Route path="/events" component={Events}/>
+                <Route path="/products" component={Products}/>
+                <Route component={Error}/>
+            </switch>
+        </div>
+    </BrowserRouter>
+</>
+
+export default Main
